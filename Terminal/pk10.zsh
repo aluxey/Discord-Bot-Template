edce5f5ec7199b94e66136c0f0e9807babceaff7
Aurelien LUXEY <aurelien.luxeyy@gmail.com>
2025-07-23 11:19:30 +0200

    Update pk10.zsh

--- Terminal/pk10.zsh
+++ Terminal/pk10.zsh
@@ -9,7 +9,7 @@ typeset -g POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX='%F{242}↳%f '
 typeset -g POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX='❯ '
 
 # OS icon
-typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION=''
+typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION=''
 
 # Couleurs Catppuccin (Mocha)
 typeset -g POWERLEVEL9K_BACKGROUND=NONE
@@ -23,13 +23,79 @@ typeset -g POWERLEVEL9K_STATUS_FOREGROUND=160
 typeset -g POWERLEVEL9K_STATUS_BACKGROUND=235
 typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_FOREGROUND=178
 typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_BACKGROUND=235
-typeset -g POWERLEVEL9K_VCS_CLEAN_FOREGROUND=70
-typeset -g POWERLEVEL9K_VCS_CLEAN_BACKGROUND=236
-typeset -g POWERLEVEL9K_VCS_DIRTY_FOREGROUND=166
-typeset -g POWERLEVEL9K_VCS_DIRTY_BACKGROUND=236
 
 # Git
-typeset -g POWERLEVEL9K_VCS_BRANCH_ICON=' '
+typeset -g POWERLEVEL9K_VCS_CLEAN_BACKGROUND=2
+typeset -g POWERLEVEL9K_VCS_MODIFIED_BACKGROUND=3
+typeset -g POWERLEVEL9K_VCS_UNTRACKED_BACKGROUND=2
+typeset -g POWERLEVEL9K_VCS_CONFLICTED_BACKGROUND=3
+typeset -g POWERLEVEL9K_VCS_LOADING_BACKGROUND=8
+
+typeset -g POWERLEVEL9K_VCS_BRANCH_ICON='\uF126 '
+typeset -g POWERLEVEL9K_VCS_UNTRACKED_ICON='?'
+
+function my_git_formatter() {
+  emulate -L zsh
+
+  if [[ -n $P9K_CONTENT ]]; then
+    typeset -g my_git_format=$P9K_CONTENT
+    return
+  fi
+
+  local       meta='%7F'
+  local      clean='%0F'
+  local   modified='%0F'
+  local  untracked='%0F'
+  local conflicted='%1F'
+
+  local res
+
+  if [[ -n $VCS_STATUS_LOCAL_BRANCH ]]; then
+    local branch=${(V)VCS_STATUS_LOCAL_BRANCH}
+    (( $#branch > 32 )) && branch[13,-13]="…"
+    res+="${clean}${(g::)POWERLEVEL9K_VCS_BRANCH_ICON}${branch//\%/%%}"
+  fi
+
+  if [[ -n $VCS_STATUS_TAG && -z $VCS_STATUS_LOCAL_BRANCH ]]; then
+    local tag=${(V)VCS_STATUS_TAG}
+    (( $#tag > 32 )) && tag[13,-13]="…"
+    res+="${meta}#${clean}${tag//\%/%%}"
+  fi
+
+  [[ -z $VCS_STATUS_LOCAL_BRANCH && -z $VCS_STATUS_TAG ]] && res+="${meta}@${clean}${VCS_STATUS_COMMIT[1,8]}"
+
+  if [[ -n ${VCS_STATUS_REMOTE_BRANCH:#$VCS_STATUS_LOCAL_BRANCH} ]]; then
+    res+="${meta}:${clean}${(V)VCS_STATUS_REMOTE_BRANCH//\%/%%}"
+  fi
+
+  if [[ $VCS_STATUS_COMMIT_SUMMARY == (|*[^[:alnum:]])(wip|WIP)(|[^[:alnum:]]*) ]]; then
+    res+=" ${modified}wip"
+  fi
+
+  (( VCS_STATUS_COMMITS_BEHIND )) && res+=" ${clean}⇣${VCS_STATUS_COMMITS_BEHIND}"
+  (( VCS_STATUS_COMMITS_AHEAD && !VCS_STATUS_COMMITS_BEHIND )) && res+=" "
+  (( VCS_STATUS_COMMITS_AHEAD  )) && res+="${clean}⇡${VCS_STATUS_COMMITS_AHEAD}"
+  (( VCS_STATUS_PUSH_COMMITS_BEHIND )) && res+=" ${clean}⇠${VCS_STATUS_PUSH_COMMITS_BEHIND}"
+  (( VCS_STATUS_PUSH_COMMITS_AHEAD && !VCS_STATUS_PUSH_COMMITS_BEHIND )) && res+=" "
+  (( VCS_STATUS_PUSH_COMMITS_AHEAD  )) && res+="${clean}⇢${VCS_STATUS_PUSH_COMMITS_AHEAD}"
+  (( VCS_STATUS_STASHES        )) && res+=" ${clean}*${VCS_STATUS_STASHES}"
+  [[ -n $VCS_STATUS_ACTION     ]] && res+=" ${conflicted}${VCS_STATUS_ACTION}"
+  (( VCS_STATUS_NUM_CONFLICTED )) && res+=" ${conflicted}~${VCS_STATUS_NUM_CONFLICTED}"
+  (( VCS_STATUS_NUM_STAGED     )) && res+=" ${modified}+${VCS_STATUS_NUM_STAGED}"
+  (( VCS_STATUS_NUM_UNSTAGED   )) && res+=" ${modified}!${VCS_STATUS_NUM_UNSTAGED}"
+  (( VCS_STATUS_NUM_UNTRACKED  )) && res+=" ${untracked}${(g::)POWERLEVEL9K_VCS_UNTRACKED_ICON}${VCS_STATUS_NUM_UNTRACKED}"
+  (( VCS_STATUS_HAS_UNSTAGED == -1 )) && res+=" ${modified}─"
+
+  typeset -g my_git_format=$res
+}
+functions -M my_git_formatter 2>/dev/null
+
+typeset -g POWERLEVEL9K_VCS_MAX_INDEX_SIZE_DIRTY=-1
+typeset -g POWERLEVEL9K_VCS_DISABLED_WORKDIR_PATTERN='~'
+typeset -g POWERLEVEL9K_VCS_DISABLE_GITSTATUS_FORMATTING=true
+typeset -g POWERLEVEL9K_VCS_CONTENT_EXPANSION='${$((my_git_formatter()))+${my_git_format}}'
+typeset -g POWERLEVEL9K_VCS_{STAGED,UNSTAGED,UNTRACKED,CONFLICTED,COMMITS_AHEAD,COMMITS_BEHIND}_MAX_NUM=-1
+typeset -g POWERLEVEL9K_VCS_BACKENDS=(git)
 
 # Exécution longue
 typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_THRESHOLD=1
